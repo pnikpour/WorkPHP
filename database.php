ba05927cd4985e266ccc58748db4bb07e8611a76
unknown <PMNikpour@L-LR036AAM.MUSD.MPSAZ.ORG>
2015-05-06 15:16:51 -0700

    Added secondary form for ticket submit

--- database.php
+++ database.php
@@ -1,33 +1,63 @@
+<html>
+<head>
+</head>
+<body>
+
+<h1>Form</h1>
+
+<form action='database.php' name='login' method='post'>
+	<input type='text' readonly='true' name='ticketNumber' /><br>
+	<input type='text' name='dateCreated' />
+<!--	<textarea rows='4' cols='40' name='problemDescription' />-->
+	<select name='problemCode'>
+		<option></option>
+		<option value="INTERNET">INTERNET</option>
+		<option value="BOOT ISSUE">BOOT ISSUE</option>
+		<option value="INSTALL">INSTALL</option>
+	</select>
+	<select name='assignedTo'>
+		<option value="pnikpour">pnikpour</option>
+	</select>
+	<input type='text' name='dateCreated' />
+	<select name='status'>
+		<option></option>
+		<option value="OPEN">OPEN</option>
+		<option value="CLOSED">CLOSED</option>
+	</select><br>
+</form>
+</body>
+</html>
+
 <?php
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-$hostname = "localhost";
+// $username = $_POST['username'];
+// $password = $_POST['password'];
+// $hostname = "localhost";
 
-$db = new PDO("mysql:host=localhost;dbname=workorder;charset=utf8", $username, $password);
+// $db = new PDO("mysql:host=localhost;dbname=workorder;charset=utf8", $username, $password);
 
 
-$query = "show schemas";
-$result = $db->query($query);
+// $query = "show schemas";
+// $result = $db->query($query);
 
-if ($result->rowCount() > 0) {
-	echo "Database" . "<br>";
-	while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-		echo $row["Database"] . "<br>";
-	}
-}
-else {
-	echo "0 results";
-}
-$query = "use workorder";
-$db->query($query);
+// if ($result->rowCount() > 0) {
+	// echo "Database" . "<br>";
+	// while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+		// echo $row["Database"] . "<br>";
+	// }
+// }
+// else {
+	// echo "0 results";
+// }
+// $query = "use workorder";
+// $db->query($query);
 
-$query = "select ticketnumber from tickets";
-$result = $db->query($query);
-echo "Ticket Number" . "<br>";
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-	echo $row['ticketNumber'] . "<br>";
-}
+// $query = "select ticketnumber from tickets";
+// $result = $db->query($query);
+// echo "Ticket Number" . "<br>";
+// while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+	// echo $row['ticketNumber'] . "<br>";
+// }
 
-unset($db);
-?>
+// unset($db);
+// ?>
--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 
 <html>
 <head>
-	<?php include 'database.php';?>
 </head>
 <body>
 
-<h1>Welcome to the work order page</h1>
+<h1>Login Page</h1>
 
+<form action='database.php' name='login' method='post'>
+	<input type='text' name='username' id='username' /><br>
+	<input type='password' name='password' id='password' />
+	<input type='submit' name='login' value='Login' />
 </body>
 </html>
