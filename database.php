9f1c23c254d8ca45efa91a9f8e7ea8b947f13e64
pnikpour <pnikpour@gmail.com>
2015-05-09 11:07:16 -0700

    Changes

--- database.php
+++ database.php
@@ -1,5 +1,10 @@
 <?php
-	session_start();
+	if (!isset($_SESSION['username'])) {
+		session_start();
+	}
+//		session_unset();
+//		session_start();
+//	}
 ?>
 
 <html>
@@ -9,28 +14,48 @@
 
 <?php
 
-if (!isset($_SESSION['username'])) {
-
-	$_SESSION['username'] = $_POST['username'];
-	$_SESSION['password'] = $_POST['password'];
-
+if (isset($_POST['saveNew'])) {
+	//session_unset();
+	$username = $_SESSION['username'];
+	$password = $_SESSION['password'];
+	//echo $_SERVER['PHP_SELF'];
 }
+else
+{
+	if (!isset($_SESSION['username'])) {
+		echo 'pingas';
+		$_SESSION['username'] = $_POST['username'];
+		$_SESSION['password'] = $_POST['password'];
 
+	}
+	else {
+	//	$username = $_SESSION['username'];
+	//	$password = $_SESSION['password'];
+	}
+}
 $username = $_SESSION['username'];
 $password = $_SESSION['password'];
+$_SESSION['username'] = $username;
+$_SESSION['password'] = $password;
 $hostname = "localhost";
 
-$db = new PDO("mysql:host=localhost;dbname=workorder;charset=utf8", $username, $password);
+try {
+	$db = new PDO("mysql:host=localhost;dbname=workorder;charset=utf8", $username, $password);
+} catch (PDOException $e) {
+	echo 'ERROR: ' . $e->getMessage();
+	session_unset();
+}
 
+$_SESSION['db'] = $db;
 
 $query = "show schemas";
 $result = $db->query($query);
 
 if ($result->rowCount() > 0) {
-     // echo "Database" . "<br>";
-     // while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-     	// echo $row["Database"] . "<br>";
-     // }
+      echo "Database" . "<br>";
+      while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+     	 echo $row["Database"] . "<br>";
+      }
 }
 else {
      // echo "0 results";
@@ -42,14 +67,15 @@ $query = "select ticketnumber from tickets";
 $result = $db->query($query);
 echo "Ticket Number" . "<br>";
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-     // echo $row['ticketNumber'] . "<br>";
+      echo $row['ticketNumber'] . "<br>";
 }
 
 unset($db);
 ?>
 <h1>Form</h1>
 
-<form action='database.php' name='login' method='post'>
+<form action="database.php" name='login' method='post'>
+<input type='submit' name='saveNew' value='Save and New' />
 <table border=1>
 	<tr>
 		<th>Ticket Number</th>
@@ -64,11 +90,13 @@ unset($db);
 
 		<select name='problemCode'>
 		<?php
-		$query = mysql_query('SHOW COLUMNS FROM `tickets` WHERE field=\'problemCode\'');
-		while ($row = mysql_fetch_row($result)) {
-			foreach (explode("','", substr($row[1],6,-2)) as $val) {
-				print("<option>$val</option>");
-			}
+		$db = $_SESSION['db'];
+		$table = "tickets";
+		$col = "problemCode";
+		$sql = 'SHOW COLUMNS FROM '.$table.' WHERE field="'.$col.'"';
+		    $row = $db->query($sql)->fetch(PDO::FETCH_ASSOC);
+		    foreach(explode("','",substr($row['Type'],6,-2)) as $option) {
+		    print("<option>$option</option>");
 		}
 		?>
 		</select>
@@ -100,3 +128,5 @@ unset($db);
 </body>
 </html>
 
+<?php
+?>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-
+<?php
+	session_start();
+?>
 
 <html>
 <head>
@@ -8,8 +10,9 @@
 <h1>Login Page</h1>
 
 <form action='database.php' name='login' method='post'>
-	<input type='text' name='username' id='username' /><br>
-	<input type='password' name='password' id='password' />
+	<input type='text' name='username' /> <br>
+	<input type='password' name='password' />
 	<input type='submit' name='login' value='Login' />
+</form>
 </body>
 </html>
