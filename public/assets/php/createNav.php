995063ab93ad48958d424fd2d8245f84f209639d
Parsa Nikpour <pnikpour@gmail.com>
2015-06-11 16:44:12 -0400

    Updated forbid function

--- public/assets/php/createNav.php
+++ public/assets/php/createNav.php
@@ -1,5 +1,6 @@
 
 <?php
+	include lib.php
 	echo
 		"<nav>
 			<input type='submit' class='button' id='home' name='home' value='Home' />
@@ -7,7 +8,7 @@
 			<input type='submit' class='button' id='filters' name='filters' value='Filters' />
 			<input type='submit' class='button' name='logout' value='Log Out' />";
 
-	if (isAdmin($user)) { 
+	if (isAdmin()) { 
 		echo"
 			<script>
 				$('#filters').after(\"<input type='submit' class='button' id='addUser' name='addUser' value='Add Users' />\");
