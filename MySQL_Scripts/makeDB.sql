3d8b2289c0de80366b5289a6186ae07f91aba35b
Parsa Nikpour <pnikpour@gmail.com>
2015-06-17 16:44:56 -0400

    Fixed casing in SQL script; added verification columns so users that create their own accounts will be emailed a verification code prior to logging in

--- MySQL_Scripts/makeDB.sql
+++ MySQL_Scripts/makeDB.sql
@@ -5,15 +5,15 @@ USE workorder;
 -- Initially set privileges for secure user account
 CREATE USER secureUser@localhost;
 SET PASSWORD FOR secureUser@localhost = PASSWORD('BL3FFEE5WUsrJQnx');
-GRANT SELECT, UPDATE, INSERT on workorder.* to 'secureUser'@'localhost' IDENTIFIED BY 'BL3FFEE5WUsrJQnx';
+GRANT SELECT, UPDATE, INSERT ON workorder.* TO 'secureUser'@'localhost' IDENTIFIED BY 'BL3FFEE5WUsrJQnx';
 FLUSH PRIVILEGES;
 
 
 -- Create tickets table
 CREATE TABLE tickets (
 	ticketNumber INT NOT NULL AUTO_INCREMENT, dateCreated date, 
-	problemDescription varchar(100), requestor varchar(25), problemCode enum('BOOT ISSUE', 'NETWORK', 'HARDWARE', 'PHONE ISSUE'), assignedTo enum('pnikpour'),
-	dateClosed date, status enum('OPEN', 'CLOSED'), PRIMARY KEY(ticketNumber)
+	problemDescription varchar(100), requestor varchar(25), problemCode ENUM('BOOT ISSUE', 'NETWORK', 'HARDWARE', 'PHONE ISSUE'), assignedTo ENUM('pnikpour'),
+	dateClosed DATE, status ENUM('OPEN', 'CLOSED'), PRIMARY KEY(ticketNumber)
 );
 
 -- Create users table
@@ -21,7 +21,9 @@ CREATE TABLE users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 	username VARCHAR(25) NOT NULL,
 	hash CHAR(128) NOT NULL,
-	groups ENUM('User', 'Administrator') NOT NULL
+	groups ENUM('User', 'Administrator') NOT NULL,
+	verifyKey VARCHAR(128) NULL,
+	isVerified BOOLEAN NOT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE loginAttempts (
