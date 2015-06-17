d6216eb0505afa7b4c5c125bf2e5ac1e8d3cdf76
Parsa Nikpour <pnikpour@gmail.com>
2015-06-17 17:29:20 -0400

    Added control statement that renders the home button only if user is registering; display normal buttons if logged in

--- public/assets/php/createNav.php
+++ public/assets/php/createNav.php
@@ -26,12 +26,15 @@
 //	include('lib.php');
 	echo
 		"<nav>
-			<input type='submit' class='button' id='home' name='home' value='Home' />
+			<input type='submit' class='button' id='home' name='home' value='Home' />";
+	if (isset($_SESSION['user'])) {
+		echo"
 			<input type='submit' class='button' id='ticket' name='ticket' value='Create Work Order' />
 			<input type='submit' class='button' id='filters' name='filters' value='Filters' />
 			<input type='submit' class='button' name='logout' value='Log Out' />";
+	}
 
-	if (isAdmin()) { 
+	if (isset($_SESSION['user']) && isAdmin()) { 
 		echo"
 			<script>
 				$('#filters').after(\"<input type='submit' class='button' id='addUser' name='addUser' value='Add Users' />\");
