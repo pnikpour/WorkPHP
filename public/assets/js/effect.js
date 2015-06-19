2e2550c84c09fc44265aac0b5d9ca5be49e4f14c
Parsa Nikpour <pnikpour@gmail.com>
2015-06-19 02:15:38 -0400

    Changed mouseout button color to blue

--- public/assets/js/effect.js
+++ public/assets/js/effect.js
@@ -33,7 +33,7 @@ $('document').ready(function() {
 	});
 
 	$('.button').mouseleave(function() {
-		$(this).animate({backgroundColor: 'black'}, 'fast');
+		$(this).animate({backgroundColor: 'blue'}, 'fast');
 	});
 
 });
