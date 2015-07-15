11851dba97fce5d4b451025a9a952df3f1aaf508
Parsa Nikpour <pnikpour@gmail.com>
2015-07-14 20:14:08 -0400

    Changed button transition effect to red

--- public/assets/js/effect.js
+++ public/assets/js/effect.js
@@ -22,18 +22,12 @@
 
 $('document').ready(function() {
 
-//	$('.color').change(function() {
-//		hex = $('.color').val();
-//		$('body').css('backgroundColor', hex);
-//		$('.button').css('backgroundColor', hex);
-//	});
-
 	$('.button').mouseover(function() {
 		$(this).animate({backgroundColor: '#D0D0D0'}, 'fast');
 	});
 
 	$('.button').mouseleave(function() {
-		$(this).animate({backgroundColor: 'blue'}, 'fast');
+		$(this).animate({backgroundColor: 'red'}, 'fast');
 	});
 
 });
