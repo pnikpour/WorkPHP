c88fa9d36b6fee45de01198dd9fe7a8aa0e99e9b
Parsa Nikpour <pnikpour@gmail.com>
2015-07-22 23:59:40 -0400

    Added MIT license header in source files

--- public/menu/index.php
+++ public/menu/index.php
@@ -1,25 +1,13 @@
 <?php
-//**************************************************************************
-// This file is part of the QuikPHP project.
-// 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
-// Programmer: Parsa Nikpour 
-// Date: 16 June 2014
-// Description: 
-// 
-//**************************************************************************
+/**
+ * Menu page that also displays a dashboard containing available support tickets. By default
+ * the admin will see all tickets (open and closed) whereas non-admin user will only see open
+ * tickets created by the user.
+ *
+ * @author Parsa Nikpour <pnikpour@gmail.com>
+ * @license http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright 2015 Parsa Nikpour
+ */
 ?>
 
 <?php
