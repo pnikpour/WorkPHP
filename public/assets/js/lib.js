b829436427379854dd530323324590534b8713a4
Parsa Nikpour <pnikpour@gmail.com>
2015-08-01 14:43:00 -0400

    Remove obsolete SQL scripts; queries will be prepared and run within PHP

---
+++ public/assets/js/lib.js
@@ -0,0 +1,12 @@
+	function readyDateClosed() {
+		if ($("#status").val() == "OPEN") {
+			$("#dateClosed").val(""); $("#dateClosed").attr("readonly", true);
+		} else {
+			var today = new Date();
+			var dd = today.getDate();
+			var mm = today.getMonth()+1;
+			mm = (mm < 10 ? "0" : "") + mm;
+			var yyyy = today.getFullYear();
+			document.getElementById("dateClosed").value = yyyy + "-" + mm + "-" + dd; $("#dateClosed").attr("readonly", false);
+		}
+	}
