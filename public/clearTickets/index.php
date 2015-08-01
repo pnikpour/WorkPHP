89cc4bc285fed51645013c9560786a75021c66ca
Parsa Nikpour <pnikpour@gmail.com>
2015-08-01 14:39:28 -0400

    Implement web interface for admin to clear entries from table

--- public/clearTickets/index.php
+++ public/clearTickets/index.php
@@ -29,11 +29,11 @@
 	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js?<?php echo time(); ?>"></script>
 	<script src='../assets/js/effect.js?<?php echo time(); ?>' type='text/javascript'></script>
 	<link rel='stylesheet' href='../assets/css/styles.css?<?php echo time(); ?>' type='text/css' />
-	<title>Clear Tickets</title>
+	<title>Clear Table - Confirmation</title>
 </head>
 <body>
 <header><img class='logo' src='../assets/images/logo.png' alt='QuikPHP Logo'></header>
-<h1>Add Users</h1>
+<h1>Clear Tickets</h1>
 
 <form action='' name='clearTickets' id='clearTickets' method='post'>
 
@@ -46,32 +46,12 @@
 
 <?php
 
-	// Create the user
-	if (isset($_POST['saveNew']) || isset($_POST['submit'])) {
-		$newName = $_POST['newName'];
-		$newPassword = $_POST['newPassword'];
-		$userLevel = $_POST['userLevel']; // Administrator or regular user
-		
-		if (userExists($newName)) { // If username exists, prompt error
-			echo 'User already exists; please specify a different username';
-			return;
-		} else // If username entered is empty, prompt error
-		if (strlen(trim($newName)) == 0) {
-			echo 'Please specify a username';
-			return;
-		} 
-
-		// Perform password add; check requirements; only checks password length
-		if (meetsPasswordLength($newPassword, true)) {
-			$hash = password_hash($newPassword, PASSWORD_DEFAULT);
-			$query = "INSERT INTO users (username, hash, groups) values (:newName, :hash, :userLevel)";
-			$result = getDB()->prepare($query);
-			$result->bindParam(':newName', $newName);
-			$result->bindParam(':hash', $hash);
-			$result->bindParam(':userLevel', $userLevel);
-			$result->execute();
-			printSuccess('User ' . $newName . ' added to database');
-		}
+	// Clear the table
+	if (isset($_POST['submit'])) {
+		$query = "DELETE FROM tickets";
+		$result = getDB()->prepare($query);
+		$result->execute();
+		printSuccess('Tickets table cleared successfully');
 	}
 
 	// Navigation link action controller
