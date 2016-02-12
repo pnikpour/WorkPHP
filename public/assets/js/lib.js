0edd46e7930b86232e797594d899969a599bf7d2
Parsa Nikpour <pnikpour@gmail.com>
2016-02-12 10:59:01 -0500

    Corrected syntax errors

--- public/assets/js/lib.js
+++ public/assets/js/lib.js
@@ -10,19 +10,18 @@ function readyDateClosed() {
 		mm = (mm < 10 ? "0" : "") + mm;
 		var yyyy = today.getFullYear();
 		document.getElementById("dateClosed").value = yyyy + "-" + mm + "-" + dd; $("#dateClosed").attr("readonly", false);
+	}
 }
 
 function addTaskRow() {
 	var numRows = $('#taskTable tr').length;
 	var table = $('#taskTable');
 
-	var newRow = table.insertRow(numRows);
-	var cell1 = newRow.insertCell(0);
-	var cell2 = newRow.insertCell(1);
-	var cell3 = newRow.insertCell(2);
+	var row = $('<tr>');
+	row.append($("<td><input type='text' name='taskName'" + numRows + "id='taskName'" + numRows + "/></td>"));
+	row.append($("<td><textarea rows='5' name='taskDescription'" + numRows + "id='taskDescription'" + numRows + "></textarea></td>"));
+	row.append($("<td><input type='checkbox' name='taskCompleted'" + numRows + "id='taskCompleted'" + numRows + "/></td>"));
 
-	cell1.innerHTML = "<input type='text' name='taskName'" + numRows + "id='taskName'" + numRows + "/>";
-	cell2.innerHTML = "<textarea rows='5' name='taskDescription'" + numRows + "id='taskDescription'" + numRows + "></textarea>";
-	cell3.innerHTML = "<input type='checkbox' name='taskCompleted'" + numRows + "id='taskCompleted'" + numRows + "/>"
+	$('#taskTable').append(row);
 
 }
