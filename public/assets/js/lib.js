2e10ed7035850a623a92e2dbd5d6947541eb80fc
Parsa Nikpour <pnikpour@gmail.com>
2016-02-16 10:21:55 -0500

    Re-add addTaskRow() function

--- public/assets/js/lib.js
+++ public/assets/js/lib.js
@@ -12,3 +12,16 @@ function readyDateClosed() {
 		document.getElementById("dateClosed").value = yyyy + "-" + mm + "-" + dd; $("#dateClosed").attr("readonly", false);
 	}
 }
+
+function addTaskRow() {
+	var numRows = $('#taskTable tr').length;
+	var table = $('#taskTable');
+
+	var row = $('<tr>');
+	row.append($("<td><input type='text' name='taskName'" + numRows + "id='taskName'" + numRows + "/></td>"));
+	row.append($("<td><textarea rows='5' name='taskDescription'" + numRows + "id='taskDescription'" + numRows + "></textarea></td>"));
+	row.append($("<td><input type='checkbox' name='taskCompleted'" + numRows + "id='taskCompleted'" + numRows + "/></td>"));
+
+	$('#taskTable').append(row);
+
+}
